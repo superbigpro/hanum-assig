3dd383305370972cd057b2e9a4a8d40fde5f28ff
leeyeonwoo <rexrex1105@gmail.com>
2023-11-23 19:57:35 +0900

    databsse modified

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,18 +1,25 @@
 from flask import Flask, redirect, request, json, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_restx import Api, Resource
 
 app = Flask(__name__)
-db = SQLAlchemy()
+api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:dldusdn1105@127.0.0.1:5000/intern_board'  # 본인의 데이터베이스 URI로 바꿔주세요
+db = SQLAlchemy(app)
 
-class Timetable(db.Model):
-    id = db.Column(db.Integer, primary_key=True) # 고유 id
-    clock = db.Column(db.String(10), nullable=False) # 시간
-    time = db.Column(db.String(10), nullable=False) # 교시 
-    content = db.Column(db.String(10), nullable=False) # 교과목 
+class Board(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True) # 고유 id
+    title = db.Column(db.String(100), nullable=False, unique=True) # 제목
+    content = db.Column(db.String(2000), nullable=False) # 게시글 내용 
+    author = db.Column(db.String(20), nullable=False) # 작성자
+    password = db.Column(db.String(25), nullable=False) # 작성자 비밀번호
+    commentId = db.collumn(db.Interger, nullable=False) #
+    comments = db.Column(db.Interger, nullable=False) # 댓글 갯수 (필수 X)
+    addedDate = db.Column(db.String(30), nullable=False) # 생성 날짜 (필수 X)
     
 def get_user_by_email(clock):
-    return Timetable.query.filter_by(clock=clock).first()
+    return Board.query.filter_by(clock=clock).first()
 
 @app.route('/posts', methods=['GET','POST'])
 def getjson():
@@ -20,8 +27,9 @@ def getjson():
         data = {"dsa":"dsafgwWDFDWWDWD"}
         return jsonify(data)
     
-    
-
+    if request.method == 'POST':
+        pass
+        
 if __name__ == "__main__":
     app.run(debug = True)
     
