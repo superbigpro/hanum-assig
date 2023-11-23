312bf42f3dd2b8059e04a63e630da51e0d971bed
leeyeonwoo <rexrex1105@gmail.com>
2023-11-23 20:42:01 +0900

    pymysql + sqlalchemy 수정 완료

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,35 +1,43 @@
-from flask import Flask, redirect, request, json, jsonify
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_restx import Api, Resource
 
 app = Flask(__name__)
-api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:dldusdn1105@127.0.0.1:5000/intern_board'  # 본인의 데이터베이스 URI로 바꿔주세요
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:dldusdn1105@127.0.0.1:5000/intern_board'
 db = SQLAlchemy(app)
 
 class Board(db.Model):
-    id = db.Column(db.Integer, primary_key=True, unique=True) # 고유 id
-    title = db.Column(db.String(100), nullable=False, unique=True) # 제목
-    content = db.Column(db.String(2000), nullable=False) # 게시글 내용 
-    author = db.Column(db.String(20), nullable=False) # 작성자
-    password = db.Column(db.String(25), nullable=False) # 작성자 비밀번호
-    commentId = db.collumn(db.Interger, nullable=False) #
-    comments = db.Column(db.Interger, nullable=False) # 댓글 갯수 (필수 X)
-    addedDate = db.Column(db.String(30), nullable=False) # 생성 날짜 (필수 X)
-    
+    __tablename__ = 'boards'
+
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    title = db.Column(db.String(100), nullable=False, unique=True)
+    content = db.Column(db.String(2000), nullable=False)
+    author = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(25), nullable=False)
+    commentId = db.Column(db.Integer, nullable=False)
+    comments = db.Column(db.Integer, nullable=False)
+    addedDate = db.Column(db.String(30), nullable=False)
+
+    def __init__(self, title, content, author, password, commentId, comments, addedDate):
+        self.title = title
+        self.content = content
+        self.author = author
+        self.password = password
+        self.commentId = commentId
+        self.comments = comments
+        self.addedDate = addedDate
+
 def get_user_by_email(clock):
     return Board.query.filter_by(clock=clock).first()
 
-@app.route('/posts', methods=['GET','POST'])
+@app.route('/posts', methods=['GET', 'POST'])
 def getjson():
     if request.method == 'GET':
-        data = {"dsa":"dsafgwWDFDWWDWD"}
+        data = {"dsa": "dsafgwWDFDWWDWD"}
         return jsonify(data)
-    
+
     if request.method == 'POST':
         pass
-        
+
 if __name__ == "__main__":
-    app.run(debug = True)
-    
+    app.run(debug=True)
