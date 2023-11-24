58bb284e3ac1ad7661aae36a0ed03753c6360850
leeyeonwoo <rexrex1105@gmail.com>
2023-11-24 20:36:53 +0900

    has db issue (for save)

--- flask-server/app.py
+++ flask-server/app.py
@@ -2,10 +2,13 @@ from flask import Flask, request, jsonify, json, make_response
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pymysql
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:dldusdn1105@127.0.0.1:5000/intern_board'
+# 기존의 import 및 설정 부분
+
 db = SQLAlchemy(app)
 
 class Board(db.Model):
@@ -16,53 +19,56 @@ class Board(db.Model):
     content = db.Column(db.String(2000), nullable=False)
     author = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(25), nullable=False)
-    commentId = db.Column(db.Integer, nullable=False)
-    comments = db.Column(db.Integer, nullable=False)
-    addedDate = db.Column(db.String(30), nullable=False)
+    # commentId = db.Column(db.Integer, nullable=False)
+    # comments = db.Column(db.Integer, nullable=False)
+    # addedDate = db.Column(db.String(30), nullable=False)
 
     def __init__(self, title, content, author, password, commentId, comments, addedDate):
         self.title = title
         self.content = content
         self.author = author
         self.password = password
-        self.commentId = commentId
-        self.comments = comments
-        self.addedDate = addedDate
+        # self.commentId = commentId
+        # self.comments = comments
+        # self.addedDate = addedDate
+    
+    def __repr__(self):
+        return '<Board %r>' % self.title
 
-def get_user_by_email(clock):
-    return Board.query.filter_by(clock=clock).first()
 
 @app.route('/posts', methods=['GET', 'POST'])
 def getjson():
-    if request.method == 'GET':
+    if request.method == 'GET': # 테스트 코드 (작동함)
         data = {"dsa": "dsafgwWDFDWWDWD"}
         return jsonify(data)
 
     if request.method == 'POST':
-         data = json.loads(request.data) # 요청 data 가져오기
+        data = request.json  # 요청에서 JSON 데이터 가져오기
 
-    params = ['title', 'content'] # tabla column 체크
+        # 요청에 필요한 파라미터가 있는지 확인
+        params = ['title', 'content', 'author', 'password']
+        for param in params:
+            if param not in data:
+                return make_response(jsonify('파라미터가 부족합니다.'), 400)
 
-    for param in params:
-        if param not in data:
-            return make_response(jsonify('Parameters are not enough.'), 400)
+        new_board = Board(
+            title=data['title'],
+            content=data['content'],
+            author=data.get('author', ''),  # 필요에 따라 작성자는 선택적으로 처리 가능
+            password=data.get('password', ''),  # 필요에 따라 비밀번호도 선택적으로 처리 가능
+            # commentId=data.get('commentId', 0),  # 필요에 따라 0 또는 다른 기본값으로 처리 가능
+            # comments=data.get('comments', 0),
+            # addedDate=datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # 현재 시간으로 처리
+        )
 
+        db.session.add(new_board)  # 새로운 데이터 추가
+        db.session.commit()  # 커밋
 
+        result = {
+            "result": "OK"
+        }
 
-    Board = boards()
-
-    Board.groupname = data['groupname']
-    Board.permission = data['permission']
-
-
-    db.session.add(Board) # db에 추가
-    db.session.commit()   # commit
-
-    result = {
-      "result": "OK"
-    }
-
-    return make_response(jsonify(result), 200)      
+        return make_response(jsonify(result), 200)
 
 if __name__ == "__main__":
     app.run(debug=True)
